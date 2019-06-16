16f9a4296495ee70ad13484bed5d83f2eff98902
chengweiou <chengwei.ou.dora@gmail.com>
2019-06-15 17:26:35 -0700

    uat path

--- uat.sh
+++ uat.sh
@@ -1,9 +1,9 @@
 ./gradlew bootJar
-cp build/libs/leonids-0.0.1-SNAPSHOT.jar ~/Desktop/docker/leonids/ser.jar
-mkdir ~/Desktop/docker/leonids/config/
-cp -r src/main/resources/leonids-bd12a-firebase-adminsdk-nna1r-246e2e1f84.json ~/Desktop/docker/leonids/config/
-cp src/main/resources/application-uat.yml ~/Desktop/docker/leonids/config/
-cp src/main/resources/log4j2.xml ~/Desktop/docker/leonids/config/
-cd ~/Desktop/docker/leonids
+cp build/libs/leonids-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universe/leonids/ser.jar
+mkdir ~/Desktop/docker/universe/leonids/config/
+cp -r src/main/resources/leonids-bd12a-firebase-adminsdk-nna1r-246e2e1f84.json ~/Desktop/docker/universe/leonids/config/
+cp src/main/resources/application-uat.yml ~/Desktop/docker/universe/leonids/config/
+cp src/main/resources/log4j2.xml ~/Desktop/docker/universe/leonids/config/
+cd ~/Desktop/docker/universe/leonids
 docker stop leonids
-docker run --rm -it -d --name leonids -p 60004:8906 --network net -v /Users/chengweiou/Desktop/docker/leonids:/proj/ -w /proj/ openjdk java -jar ser.jar
+docker run --rm -it -d --name leonids -p 60004:8906 --network net -v /Users/chengweiou/Desktop/docker/universe/leonids:/proj/ -w /proj/ openjdk java -jar ser.jar
