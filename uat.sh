a41b6fa0140af600e693d3cced82e12eff83acca
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-10-04 16:26:51 -0700

    fcm.json

--- uat.sh
+++ uat.sh
@@ -1,7 +1,7 @@
 ./gradlew bootJar
 cp build/libs/leob-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universe/leob/ser.jar
 mkdir ~/Desktop/docker/universe/leob/config/
-cp -r src/main/resources/fcm/ ~/Desktop/docker/universe/leob/config/
+cp -r src/main/resources/fcm/leob-key.json ~/Desktop/docker/universe/leob/config/
 #cp src/main/resources/application.yml ~/Desktop/docker/universe/leob/config/
 cp src/main/resources/application-uat.yml ~/Desktop/docker/universe/leob/config/
 cp src/main/resources/log4j2.xml ~/Desktop/docker/universe/leob/config/
